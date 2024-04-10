9ff1a0505ebf85e6d0af7dcef5e6ca505ee95029
Sam Wardle <samuelwardle@hotmail.com>
2024-04-10 17:28:59 +0100

    infobox text colour changed

--- Assets/Scripts/PlanetInfo.cs
+++ Assets/Scripts/PlanetInfo.cs
@@ -8,11 +8,13 @@ public class PlanetInfo : MonoBehaviour {
 
     protected void OnHoverEntered()
     {
+        print("Jupiter Hover Entered");
         infobox.SetActive(true);
     }
 
     protected void OnHoverExited()
     {
+        print("Jupiter Hover Exited");
         infobox.SetActive(false);
     }
 }
